23e5e1d4580bb326a91e482e7c47f4f32f0567ae
eber <eber.penaloza10@gmail.com>
2025-04-02 23:08:25 -0500

    agregar lista:recorrido de una lista

---
+++ recorrido_lista.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+struct Nodo{
+    int inf;
+    Nodo* sig;
+};
+int main(){
+    Nodo* p=nullptr;  // Lista vacía
+
+    //primer nodo
+    Nodo* q1= new Nodo();
+    q1->inf=10;
+    q1->sig=p;
+    p=q1;
+    // segundo nodo
+    Nodo* q2= new Nodo();
+    q2->inf=20;
+    q2->sig=p;
+    p=q2;
+    // tercer nodo
+    Nodo* q3=new Nodo();
+    q3->inf=30;
+    q3->sig=p;
+    p=q3;
+    Nodo* q=p;
+    while (q!= nullptr) {
+        cout << q->inf<< " ";
+        q=q->sig;
+    }
+    return 0;
+}
